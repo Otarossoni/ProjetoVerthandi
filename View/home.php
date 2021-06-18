18e9e2534c89c22e5f2753fe7b148a55ea7f60fc
Daniel Conte <182736@upf.br>
2021-06-17 22:28:57 -0300

    Estiliza a tela de bem vindo

--- View/home.php
+++ View/home.php
@@ -5,34 +5,23 @@ $user = unserialize($_SESSION['user']);
 if (!$user)
     header("location:../index.php");
 ?>
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<header class="header d-flex flex-column px-3 bg-white">
+  <h1 class="mt-3"><i class="fa fa-home pr-2"></i>Bem-vindo, Você!</h1>
+  <p class="lead text-muted">Verthandi é um projeto de caráter educacional, desenvolvido durante o primeiro período letivo do ano de 2021 na disciplina de “Desenvolvimento para Web”.</p>
+</header>
 
-<body>
-    <div style="padding: 0.5%;">
-    <h1>Bem-vindo, Você!</h1><br>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Verthandi é um projeto de caráter educacional, desenvolvido durante o primeiro período letivo do ano de
-        2021 na disciplina de “Desenvolvimento para Web”.</p>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Verthandi funciona como um catálogo pessoal de mídias, sendo elas audiovisuais ou não, aqui você pode cadastrar vários tipos de mídias,
-        desde livros e jornais, até filmes, séries e animes. Mas você adiciona somente aquelas que lhe apetecerem. Autores também podem ser adicionados, eles podem ser realmente autores,
-        mas também representam roteiristas, escritores e etc. O projeto é básico, mas serve como método de aperfeiçoamento daqueles por detrás do desenvolvimento.</p>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Abaixo, acesso ao GitHub de cada integrante, e o repositório do projeto:</p>
-    <ul>
-        <li>Verthandi: <a href="https://github.com/Otarossoni/ProjetoVerthandi">https://github.com/Otarossoni/ProjetoVerthandi</a></li>
-        <li>Otávio Monteiro Rossoni: <a href="https://github.com/Otarossoni">https://github.com/Otarossoni</a></li>
-        <li>Daniel Conte: <a href="https://github.com/Daniel-Conte">https://github.com/Daniel-Conte</a></li>
-    </ul>
-    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<label style="font-weight: bold;">TRIVIA: </label> O nome Verthandi é baseado em uma das três normas nórdicas que regem o destino dos homens,
-    Verthandi é a deusa do presente. Aproveite o seu, vá consumir algumas mídias. :)</p>
-    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;El Psy Kongroo</p>
+<main class="content container-fluid">
+    <div class="p-3 mt-3 bg-white">
+        <p>Verthandi funciona como um <strong>catálogo pessoal de mídias</strong>, sendo elas audiovisuais ou não, aqui você pode cadastrar vários tipos de mídias, desde livros e jornais, até filmes, séries e animes. Mas você adiciona somente aquelas que lhe apetecerem. Autores também podem ser adicionados, eles podem ser realmente autores, mas também representam roteiristas, escritores e etc. O projeto é básico, mas serve como método de aperfeiçoamento daqueles por detrás do desenvolvimento</p>
+        <p>Abaixo, acesso ao GitHub de cada integrante, e o repositório do projeto:</p>
+        <ul>
+            <li><strong>Verthandi:</strong> <a href="https://github.com/Otarossoni/ProjetoVerthandi">https://github.com/Otarossoni/ProjetoVerthandi</a></li>
+            <li><strong>Otávio Monteiro Rossoni:</strong> <a href="https://github.com/Otarossoni">https://github.com/Otarossoni</a></li>
+            <li><strong>Daniel Conte:</strong> <a href="https://github.com/Daniel-Conte">https://github.com/Daniel-Conte</a></li>
+        </ul>
+        <p><strong>TRIVIA:</strong> O nome Verthandi é baseado em uma das três normas nórdicas que regem o destino dos homens, Verthandi é a deusa do presente. Aproveite o seu, vá consumir algumas mídias. :)</p>
+        
+        <p class="mt-5">El Psy Kongroo</p>
     </div>
-</body>
-
-</html>
+</main>

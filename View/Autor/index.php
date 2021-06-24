4376b35ba88bbdc2579a47b3135cf2b07133b48f
Daniel Conte <182736@upf.br>
2021-06-23 21:53:04 -0300

    habilita o alterar de autores no front

--- View/Autor/index.php
+++ View/Autor/index.php
@@ -14,25 +14,55 @@
     <div class="p-3 mt-3 bg-white">
         <form action="../Controller/AutorController.php?operation=cadastrar" class="form" method="post" name="form_autor">
             <div class="row">
+                <div class="form-group text-left col-1">
+                    <label>ID: </label>
+                    <?php
+                        $id = '';
+                        if(isset($_SESSION['autor'])) {
+                            $id = unserialize($_SESSION['autor'])[0]['idautor'];
+                        }
+                        echo "<input readonly type='text' name='id' id='id' class='form-control' value='$id'/>"
+                    ?>
+                </div>
                 <div class="form-group text-left col-3">
                     <label>Nome: </label>
-                    <input required type="text" name="nome" id="nome" class="form-control" placeholder="Digite o nome..."/>
+                    <?php
+                        $nome = '';
+                        if(isset($_SESSION['autor'])) {
+                            $nome = unserialize($_SESSION['autor'])[0]['nome'];
+                        }
+                        echo "<input required type='text' name='nome' id='nome' class='form-control' placeholder='Digite o nome...' value='$nome'/>";
+                    ?>
                 </div>
                 <div class="form-group text-left col-3">
                     <label>Tipo: </label>
-                    <input required type="text" name="tipo" id="tipo" class="form-control" placeholder="Digite o tipo..."/>
+                    <?php
+                        $tipo = '';
+                        if(isset($_SESSION['autor'])) {
+                            $tipo = unserialize($_SESSION['autor'])[0]['tipo'];
+                        }
+                        echo "<input required type='text' name='tipo' id='tipo' class='form-control' placeholder='Digite o tipo...' value='$tipo'/>";
+                    ?>
                 </div>
                 <div class="form-group text-left col-5">
                     <label>Descrição: </label>
-                    <textarea required type="text" name="descricao" id="descricao" cols="20" rows="3" class="form-control" placeholder="Digite a descrição..."></textarea>
+                    <?php
+                        $descricao = '';
+                        if(isset($_SESSION['autor'])) {
+                            $descricao = unserialize($_SESSION['autor'])[0]['descricao'];
+                        }
+                        echo "<textarea required type='text' name='descricao' id='descricao' cols='20' rows='3' class='form-control' placeholder='Digite a descrição...'>$descricao</textarea>";
+
+                        unset($_SESSION['autor'])
+                    ?>
                 </div>
                 
             </div>
 
             <div class="row">
                 <div class="col-12 d-flex justify-content-end">
-                    <input type="submit" value="Inserir" class="btn primary mr-2">
-                    <input type="reset" value="Limpar" class="btn btn-secondary">
+                    <input type="submit" value="Salvar" class="btn primary mr-2">
+                    <a href="?page=autor" class="btn btn-secondary">Limpar</a>
                 </div>
             </div>
         </form>
@@ -65,7 +95,7 @@
                                     <td>$tipo</td>
                                     <td>$descricao</td>
                                     <td>
-                                        <a href='../Controller/AutorController.php?operation=atualizar&id=$id' class='btn btn-warning'><i class='fas fa-pencil-alt pr-2'></i>Alterar</a>
+                                        <a href='../Controller/AutorController.php?operation=consultar&id=$id' class='btn btn-warning'><i class='fas fa-pencil-alt pr-2'></i>Alterar</a>
                                         <a href='../Controller/AutorController.php?operation=deletar&id=$id' class='btn btn-danger'><i class='fa fa-trash pr-2'></i>Deletar</a>
                                     </td>
                                 </tr>";
